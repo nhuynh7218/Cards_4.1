7884a0339001338d524a1f8077341b390b5b99bc
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-03-13 09:29:14 -0400

    ag

--- Cardz/src/Cards.java
+++ Cardz/src/Cards.java
@@ -1,4 +1,17 @@
 
 public class Cards {
+	String rank;
+	String suit;
+	int pointValue;
 	
+	public Cards(String rank, String suit, int pointValue)
+	{
+		this.rank = rank;
+		this.suit = suit;
+		
+	}
+	public Deck(String [] rank, String [] suit, int pointValue)
+	{
+		
+	}
 }
