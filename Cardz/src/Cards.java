a4762d95e084ed5acf15117bebbc2108ba7ebcd0
BT_5N4_32 <BT_5N4_32@BT_5N4_32.bktech.local>
2018-03-13 14:14:51 -0400

    runer class

--- Cardz/src/Cards.java
+++ Cardz/src/Cards.java
@@ -1,17 +1,45 @@
 
 public class Cards {
+	//fields
 	String rank;
 	String suit;
 	int pointValue;
 	
+	//Constructor
 	public Cards(String rank, String suit, int pointValue)
 	{
 		this.rank = rank;
 		this.suit = suit;
 		this.pointValue = pointValue;
 	}
-	public Deck(String [] rank, String [] suit, int pointValue)
+	//getters and setters for the fields
+	public String getRank() {
+		return rank;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	public int getPointValue() {
+		return pointValue;
+	}
+
+	public void setPointValue(int pointValue) {
+		this.pointValue = pointValue;
+	}
+	
+	public boolean equals()
 	{
 		
 	}
+	
 }
